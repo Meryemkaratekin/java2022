9a2ea74a1669136395fa7ff513b4e656d352d681
meryem <105595498+Meryemkaratekin@users.noreply.github.com>
2022-10-03 15:41:56 +0300

    Add files via upload

---
+++ döngüler.java
@@ -0,0 +1,49 @@
+package döngüler;
+
+import java.util.Iterator;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		int i ;
+		for (i=1;i<10;i++) {
+			System.out.println(i);
+		}
+		    System.out.println("For döngüsü bitti");
+		// while döngüsü
+		
+		
+		int j=2;
+		
+        while (j<10){
+        	System.out.println(j);
+        	j++;
+        	
+        }
+        System.out.println("while döngüsü bitti");
+        // do while döngüsü // do while da 
+        
+        int a =100;
+        
+        do {
+      
+        	System.out.println(a);
+    		a++;
+        	
+        } while (a<10); {
+    	
+    		System.out.println("Do while döngüsü bitti");
+    		
+    		
+    	}
+        
+			
+
+		
+			
+		
+
+	}
+
+}
