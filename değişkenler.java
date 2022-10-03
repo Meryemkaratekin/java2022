e8185eb274a67488a6afbac686c5ca37a06c673e
meryem <105595498+Meryemkaratekin@users.noreply.github.com>
2022-10-03 15:41:32 +0300

    Add files via upload

---
+++ değişkenler.java
@@ -0,0 +1,19 @@
+package değişkenler;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// javada değişken tanımlama camelCasing olaarak yapılır
+		
+		int öğrenciSAYISI = 10;
+		String metin = " Öğrenci Sayısı: ";
+		
+	    System.out.println(metin +öğrenciSAYISI);
+	    System.out.println(metin +öğrenciSAYISI);
+	    System.out.println(metin +öğrenciSAYISI);
+	    System.out.println(" Öğrenci saısı:" +öğrenciSAYISI);
+
+	}
+
+}
