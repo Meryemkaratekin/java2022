6bf589c6256c3883e6d08b6ed8c594ba4a7cc36c
meryem <105595498+Meryemkaratekin@users.noreply.github.com>
2022-10-03 15:50:28 +0300

    Add files via upload

---
+++ sesliHarf.java
@@ -0,0 +1,35 @@
+package sesliHarf;
+
+public class main {
+
+	public static void main(String[] args) {
+		char harf = 'E' ;
+		
+			switch (harf) { 
+			case 'A':
+			System.out.println("KALIN SESLİ HARF");
+			break;
+			case 'I' :
+			System.out.println("KALIN SESLİ HARF");
+			break;
+			case 'O' :
+			System.out.println("KALIN SESLİ HARF");
+			break;
+			case 'U' :
+			System.out.println("KALIN SESLİ HARF");
+			break;
+			 default :
+				 System.out.println("İNCE SESLİ HARF");
+					
+				
+				
+			
+			
+			
+				
+
+	}
+	}
+	}
+  
+    	 
