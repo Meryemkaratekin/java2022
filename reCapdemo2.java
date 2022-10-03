6152aa0e464a36e1f7d2bdbbed57ed28d924b203
meryem <105595498+Meryemkaratekin@users.noreply.github.com>
2022-10-03 15:48:46 +0300

    Add files via upload

---
+++ reCapdemo2.java
@@ -0,0 +1,24 @@
+package reCapdemo2;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		double[] myList =  { 1.2,8.3,9.5,4.8 };
+		double total = 0;
+		double max = myList[0];
+		
+		for(double number:myList) {
+			if(max<number) {
+				max=number;
+			}
+			total=total+number;
+			System.out.println(number);
+		}
+		System.out.println("Toplam:" + total);
+		System.out.println("EN BUYUK " +max);
+		
+
+	
+	}
+}
