381611557030f07c45974e0611686fbba14476cd
meryem <105595498+Meryemkaratekin@users.noreply.github.com>
2022-10-03 15:49:05 +0300

    Add files via upload

---
+++ reCapdemo1.java
@@ -0,0 +1,24 @@
+package reCapDmo1;
+
+public class main {
+
+	public static void main(String[] args) {
+	
+		 int sayi1 = 2;
+		 int sayi2 = 3;
+		 int sayi3 = 4;
+		 int enbuyuksayi=sayi1;
+	 
+		 if (enbuyuksayi<sayi2) {
+			 enbuyuksayi =sayi2;
+			
+		}
+		 if(enbuyuksayi<sayi3) {
+			 enbuyuksayi = sayi3;
+		 }
+		 
+		 
+      System.out.println("EN BÜYÜK SAYI: " +enbuyuksayi);
+	}
+
+}
